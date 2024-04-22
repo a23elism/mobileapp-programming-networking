47c2daf7e123eb4dcb8f66ecc26758945472f8c7
a23elism <a23elism@student.his.se>
2024-04-22 14:42:32 +0200

    Implemented RecycleView (adapter included)

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,26 +4,36 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private RecyclerViewAdapter adapter;
-
-    private Gson gson;
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+
+    ArrayList<Mountain> Mountains= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gson = new Gson();
+        Gson gson = new Gson();
+
+        RecyclerView recycler_view = findViewById(R.id.RecyclerView);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter (this, Mountains);
+        recycler_view.setAdapter(adapter);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
 
         new JsonFile(this, this).execute(JSON_FILE);
+
     }
 
     @Override
--- app/src/main/java/com/example/networking/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/networking/RecyclerViewAdapter.java
@@ -1,4 +1,51 @@
 package com.example.networking;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+    Context context;
+    ArrayList <Mountain> Mountains;
+    public RecyclerViewAdapter(Context context, ArrayList<Mountain> Mountains){
+        this.context =context;
+        this.Mountains=Mountains;
+    }
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view =inflater.inflate(R.layout.recycler_layout, parent, false);
+
+        return new RecyclerViewAdapter.MyViewHolder (view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.textview.setText(Mountains.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return Mountains.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView textview;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textview = itemView.findViewById(R.id.title);
+        }
+    }
 }
+
+
